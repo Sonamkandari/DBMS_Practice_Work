12ebe66b2526c40df6fc5958dd109d15815594e7
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2025-08-27 15:55:00 +0530

    Update Pratice_work2.sql

--- Pratice_work2.sql
+++ Pratice_work2.sql
@@ -1,6 +1,11 @@
+--  gave all first name and last name where wight of betients is between 100 to 120
 select first_name , last_name
 from patients 
 where weight >=100 and weight <=120;
+
+select first_name , last_name 
+from patients 
+where weight  between 100 and 120  
 
 
 --  update allergies column as 'NKA ' where allergies are null 
@@ -8,3 +13,9 @@ where weight >=100 and weight <=120;
 UPDATE patients
 set allergies ='NKA'
 where allergies is null;
+
+-- Show first name and last name concatinated into one column to show their full name.
+  -- here we can use a CONCAT in build function for adding values of two columns in a single columns
+SELECT CONCAT
+(first_name,' ',last_name) 
+from patients;
